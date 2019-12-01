afe6ab8fb732c3f839b4438a0a2bec8b22a9569c
Maximilian Leopold <max.leopold2111@gmail.com>
2019-12-01 16:20:00 +0100

    Solution Nr. 7

---
+++ src/solutions/Nr7.java
@@ -0,0 +1,25 @@
+package solutions;
+
+public class Nr7 {
+
+  public static int reverse(int x) {
+
+    int result = 0;
+    int prevResult = 0;
+
+    while(x != 0){
+      int lastDig = x % 10;
+      x = x / 10;
+      result = result * 10 + lastDig;
+      if((result - lastDig) / 10 != prevResult) return 0;
+
+      prevResult = result;
+    }
+
+    return result;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(reverse(901000));
+  }
+}

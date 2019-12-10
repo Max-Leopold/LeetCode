783d6863ffe1e33792f7ed4acee8f4f6f4cd8f9b
Maximilian Leopold <max.leopold2111@gmail.com>
2019-12-10 22:36:02 +0100

    Solution Nr. 1

---
+++ src/solutions/Nr1.java
@@ -0,0 +1,22 @@
+package solutions;
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a
+ * specific target.
+ * <p>
+ * You may assume that each input would have exactly one solution, and you may not use the same
+ * element twice.
+ */
+
+public class Nr1 {
+  public int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          return new int[] {i, j};
+        }
+      }
+    }
+    return null;
+  }
+}

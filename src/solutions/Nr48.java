0a95ef04963b37d07a23b61af7d1ab0c39a0638f
Maximilian Leopold <max.leopold2111@gmail.com>
2019-12-15 20:43:36 +0100

    Solution Nr. 48

---
+++ src/solutions/Nr48.java
@@ -0,0 +1,33 @@
+package solutions;
+
+/**
+ * You are given an n x n 2D matrix representing an image.
+ * <p>
+ * Rotate the image by 90 degrees (clockwise).
+ * <p>
+ * Note:
+ * <p>
+ * You have to rotate the image in-place, which means you have to modify
+ * the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+ */
+
+public class Nr48 {
+
+  public void rotate(int[][] matrix) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = i; j < matrix[0].length; j++) {
+        int temp = matrix[j][i];
+        matrix[j][i] = matrix[i][j];
+        matrix[i][j] = temp;
+      }
+    }
+
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix.length / 2; j++) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[i][matrix.length - 1 - j];
+        matrix[i][matrix.length - 1 - j] = temp;
+      }
+    }
+  }
+}

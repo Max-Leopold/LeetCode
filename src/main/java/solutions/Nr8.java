753f1cb8e0ab6898960629d50f5b6e985a96724f
Maximilian Leopold <max.leopold2111@gmail.com>
2019-12-16 21:57:03 +0100

    Solution Nr. 8

---
+++ src/main/java/solutions/Nr8.java
@@ -0,0 +1,44 @@
+package main.java.solutions;
+
+public class Nr8 {
+
+  public int myAtoi(String str) {
+    int result = 0;
+    int lastRes = 0;
+    boolean minus = false;
+
+    //Inefficient solution using regex
+    if (str.trim().length() == 0 || !str.trim().substring(0, 1).replaceAll("[(^\\d-+)]", "").equals("")) {
+      return 0;
+    }
+
+    str = str.trim().replaceAll("(?<!^)\\D(.?)*", "");
+
+    for (int i = 0; i < str.length(); i++) {
+      char c = str.charAt(i);
+      if (c == '-') {
+        minus = true;
+        continue;
+      } else if (c == '+') {
+        minus = false;
+        continue;
+      }
+
+      if (minus) {
+        result = result * 10 - Character.getNumericValue(c);
+        if (result / 10 != lastRes) {
+          return Integer.MIN_VALUE;
+        }
+        lastRes = result;
+      } else {
+        result = result * 10 + Character.getNumericValue(c);
+        if (result / 10 != lastRes) {
+          return Integer.MAX_VALUE;
+        }
+        lastRes = result;
+      }
+    }
+
+    return result;
+  }
+}

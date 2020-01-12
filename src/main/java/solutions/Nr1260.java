a29ff1b3cee3612437e872593ae5a3ede1c40357
Max Leopold <max.leopold2111@gmail.cm>
2020-01-12 17:24:42 +0100

    Solution Nr. 1260

---
+++ src/main/java/solutions/Nr1260.java
@@ -0,0 +1,51 @@
+package main.java.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nr1260 {
+
+  public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+    for (int i = 0; i < k; i++) {
+      shiftColumns(grid);
+    }
+
+    List<List<Integer>> list = new ArrayList<>();
+    for (int i = 0; i < grid.length; i++) {
+      List<Integer> arrList = new ArrayList<>();
+
+      for (int j = 0; j < grid[i].length; j++) {
+        arrList.add(grid[i][j]);
+      }
+      list.add(arrList);
+    }
+
+    return list;
+  }
+
+  public int[][] shiftColumns(int[][] grid) {
+    int[] temp = createLastColumnArray(grid);
+
+    for (int i = 0; i < grid.length; i++) {
+      for (int j = grid[i].length - 1; j > 0; j--) {
+        grid[i][j] = grid[i][j - 1];
+      }
+    }
+
+    for (int i = 0; i < grid.length; i++) {
+      grid[i][0] = temp[i];
+    }
+
+    return grid;
+  }
+
+  public int[] createLastColumnArray(int[][] grid) {
+    int temp[] = new int[grid.length];
+
+    for (int i = 0; i < grid.length; i++) {
+      temp[(i + 1) % grid.length] = grid[i][grid[i].length - 1];
+    }
+
+    return temp;
+  }
+}

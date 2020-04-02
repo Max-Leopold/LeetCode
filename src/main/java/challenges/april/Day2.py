f0b69ca20592c47e5e94340a416cb1f87a45ab6d
Max Leopold <max.leopold2111@gmail.com>
2020-04-02 09:50:55 +0200

    April Challenge Day 2

---
+++ src/main/java/challenges/april/Day2.py
@@ -0,0 +1,14 @@
+class Solution:
+	def isHappy(self, n: int) -> bool:
+		s = set()
+
+		while n not in s:
+			s.add(n)
+			l = list(map(int, str(n)))
+			n = 0
+			for i in l:
+				n += i * i
+			if n == 1:
+				return True
+
+		return False

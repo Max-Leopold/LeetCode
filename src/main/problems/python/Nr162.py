c24052e7a2a114d6b0811eca45569c0c0b3f3722
Max Leopold <max.leopold2111@gmail.com>
2020-04-14 18:09:45 +0200

    Nr. 162

---
+++ src/main/problems/python/Nr162.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+	def findPeakElement(self, nums: List[int]) -> int:
+		if len(nums) == 1:
+			return 0
+
+		for i in range(len(nums)):
+			if i == 0:
+				if nums[i] > nums[i + 1]:
+					return i
+			elif i == len(nums) - 1:
+				if nums[i] > nums[i - 1]:
+					return i
+			else:
+				if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
+					return i
+
+		return -1

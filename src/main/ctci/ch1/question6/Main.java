6c999f6a9df91a2d853bf0a953b54bc7236f0669
Max Leopold <max.leopold2111@gmail.com>
2021-04-11 14:55:00 +0200

    CTCI Chapter 1 Question 6

---
+++ src/main/ctci/ch1/question6/Main.java
@@ -0,0 +1,10 @@
+package main.ctci.ch1.question6;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Solution1.stringCompression("aabcccccccccccaaa"));
+
+        System.out.println(Solution2.stringCompression("aabcccccccccccaaa"));
+    }
+}

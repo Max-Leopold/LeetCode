8a7ad094386f58c26e336f2d08c7fd0951863cfe
Max Leopold <max.leopold2111@gmail.com>
2021-04-11 11:54:49 +0200

    CTCI Chapter 1 Question 4

---
+++ src/main/ctci/ch1/question4/Main.java
@@ -0,0 +1,8 @@
+package main.ctci.ch1.question4;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Solution1.palindromePermutation("Tact cao"));
+    }
+}
---
+++ src/main/ctci/ch1/question4/Solution1.java
@@ -0,0 +1,40 @@
+package main.ctci.ch1.question4;
+
+// O(n) time with O(1) space
+
+// Questions
+// Do whitespaces matter? (Assumption: no)
+// Does capitalization matter? (Assumption: no)
+// Which char set is used? (Assumption: ASCII)
+
+import java.util.Locale;
+
+public class Solution1 {
+
+    static boolean palindromePermutation(String input) {
+        if (input.length() == 0 || input.length() == 1) {
+            return true;
+        }
+
+        // If Uppercase/Lowercase is important remove this line
+        input = input.toLowerCase(Locale.ROOT);
+        int odds = 0;
+
+        int[] charCounter = new int[128];
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == ' ') {
+                continue;
+            }
+
+            charCounter[input.charAt(i)]++;
+            if (charCounter[input.charAt(i)] % 2 == 0) {
+                odds++;
+            } else {
+                odds--;
+            }
+        }
+
+        return odds <= 1;
+    }
+}

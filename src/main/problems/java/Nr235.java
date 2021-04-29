22f4a35a381369d1e882f182f193ff47455bd123
Max Leopold <max.leopold2111@gmail.com>
2021-04-29 17:24:14 +0200

    Nr.235

---
+++ src/main/problems/java/Nr235.java
@@ -0,0 +1,33 @@
+package main.problems.java;
+
+import main.util.java.TreeNode;
+
+public class Nr235 {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q) {
+            return root;
+        }
+
+        boolean pIsInRightTree = isInTree(root.right, p);
+        boolean qIsInRightTree = isInTree(root.right, q);
+        if (qIsInRightTree && pIsInRightTree) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (!qIsInRightTree && !pIsInRightTree) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            return root;
+        }
+
+    }
+
+    private boolean isInTree(TreeNode root, TreeNode searching) {
+        if (root == null) {
+            return false;
+        }
+        if (root == searching) {
+            return true;
+        }
+        return isInTree(root.right, searching) || isInTree(root.left, searching);
+    }
+}

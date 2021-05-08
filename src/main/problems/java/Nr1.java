ad1d6f029655e31237790f157116cc0087c15286
Max Leopold <max.leopold2111@gmail.com>
2021-05-08 20:55:42 +0200

    Nr.1 more efficient

--- src/main/problems/java/Nr1.java
+++ src/main/problems/java/Nr1.java
@@ -1,5 +1,7 @@
 package main.problems.java;
 
+import java.util.HashMap;
+
 /**
  * Given an array of integers, return indices of the two numbers such that they add up to a
  * specific target.
@@ -9,11 +11,25 @@ package main.problems.java;
  */
 
 public class Nr1 {
+
+  public int[] twoSumEfficient(int[] nums, int target) {
+    HashMap<Integer, Integer> numsToIndex = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int searchFor = target - nums[i];
+      if (numsToIndex.containsKey(searchFor)) {
+        return new int[]{numsToIndex.get(searchFor), i};
+      } else {
+        numsToIndex.put(nums[i], i);
+      }
+    }
+    throw new IllegalArgumentException();
+  }
+
   public int[] twoSum(int[] nums, int target) {
     for (int i = 0; i < nums.length; i++) {
       for (int j = i + 1; j < nums.length; j++) {
         if (nums[i] + nums[j] == target) {
-          return new int[] {i, j};
+          return new int[]{i, j};
         }
       }
     }

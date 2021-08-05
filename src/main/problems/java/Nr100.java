856ad67d46246da65e7e7b65b17597e7fbf7d7bf
Max Leopold <max.leopold2111@gmail.com>
2021-08-05 18:59:07 +0200

    Nr. 100

---
+++ src/main/problems/java/Nr100.java
@@ -0,0 +1,18 @@
+package main.problems.java;
+
+import main.util.java.TreeNode;
+
+public class Nr100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if ((p == null && q != null) || (p != null && q == null)) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}

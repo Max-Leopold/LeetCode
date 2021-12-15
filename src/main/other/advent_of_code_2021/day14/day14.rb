349fe3dd31b84537ec02e3cd90564e89638fcacc
Max-Leopold <max.leopold2111@gmail.com>
2021-12-15 21:55:03 +0100

    AOC Day 14 - Use custom Cacheable module

--- src/main/other/advent_of_code_2021/day14/day14.rb
+++ src/main/other/advent_of_code_2021/day14/day14.rb
@@ -1,3 +1,5 @@
+require_relative '../../../util/ruby/cache'
+
 initial_polymer = File.open(File.join(File.dirname(__FILE__), "initial_polymer.txt"), &:readline).chomp.split("")
 
 polymer_table = File.open(File.join(File.dirname(__FILE__), "polymer_template.txt"), "r").map do |line|
@@ -7,20 +9,26 @@ end.to_h
 
 CACHE = {}
 
-def next_polymer(polymer_pair, polymer_table, limit, step=0)
-    return CACHE[(polymer_pair.to_s + step.to_s).hash] unless CACHE[(polymer_pair.to_s + step.to_s).hash].nil?
-    inserted_element = polymer_table[polymer_pair]
-    return [polymer_pair[0], inserted_element].tally if limit - 1 == step
+class Polymer
+    extend Cacheable
 
-    CACHE[(polymer_pair.to_s + step.to_s).hash] = \
-    next_polymer([polymer_pair[0], inserted_element], polymer_table, limit, step + 1) \
-        .merge(next_polymer([inserted_element, polymer_pair[1]], polymer_table, limit, step + 1)) {|key, v1, v2|
-            v1 + v2
-        }
+    def next_polymer(polymer_pair, polymer_table, limit, step)
+        # return CACHE[(polymer_pair.to_s + step.to_s).hash] unless CACHE[(polymer_pair.to_s + step.to_s).hash].nil?
+        inserted_element = polymer_table[polymer_pair]
+        return [polymer_pair[0], inserted_element].tally if limit - 1 == step
+
+        # CACHE[(polymer_pair.to_s + step.to_s).hash] = \
+        next_polymer([polymer_pair[0], inserted_element], polymer_table, limit, step + 1) \
+            .merge(next_polymer([inserted_element, polymer_pair[1]], polymer_table, limit, step + 1)) {|key, v1, v2|
+                v1 + v2
+            }
+    end
+    cache :next_polymer, -> (polymer_pair, polymer_table, limit, step) {(polymer_pair.to_s + step.to_s).hash}
 end
 
+polymer = Polymer.new
 polymer_counts = initial_polymer[0..-2].zip(initial_polymer[1..-1]).map {|polymer_pair|
-    next_polymer(polymer_pair, polymer_table, 40)
+    polymer.next_polymer(polymer_pair, polymer_table, 40, 0)
 }.inject {|agg, hash|
     agg.merge(hash) {|key, v1, v2|
         v1 + v2

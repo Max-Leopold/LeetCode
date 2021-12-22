29c8cdb0aa1993c5d3635c7e8ec3c50a7aadca8d
Max Leopold <max.leopold2111@gmail.com>
2021-12-22 14:40:29 +0100

    AOC 2021 Day 22 Part 2

--- src/main/other/advent_of_code_2021/day22/day22.rb
+++ src/main/other/advent_of_code_2021/day22/day22.rb
@@ -1,18 +1,30 @@
 require_relative 'reactor'
 
-input = File.open(File.join(File.dirname(__FILE__), "input.txt"), "r")
+def execute_operations(operations, reactor)
+    operations.each {|line|
+        operation, coordinates = line.chomp.split(" ")
+        x, y, z = coordinates.split(",").map {|coord| coord[2..-1].split("..").map(&:to_i)}
+        # Increment x´, y´ and z´ by one because my implementation is checking cuboids as [x, x2)
+        x[1] += 1
+        y[1] += 1
+        z[1] += 1
+        case operation
+        when "on"
+            reactor.turn_on_cuboid([*x, *y, *z])
+        when "off"
+            reactor.turn_off_cuboid([*x, *y, *z])
+        end
+    }
+end
 
-reactor = Reactor.new
-input.each {|line|
-    operation, coordinates = line.chomp.split(" ")
-    x, y, z = coordinates.split(",").map {|coord| coord[2..-1].split("..").map(&:to_i)}
-    case operation
-    when "on"
-        reactor.turn_on_cuboid(*x, *y, *z)
-    when "off"
-        reactor.turn_off_cuboid(*x, *y, *z)
-    end
-}
+input_part1 = File.open(File.join(File.dirname(__FILE__), "part1-input.txt"), "r")
+reactor_part1 = Reactor.new
+execute_operations(input_part1, reactor_part1)
 
-puts reactor.on_cubes.length
-(-1..2).each {|a| puts a}
+puts "Day 22, Part 1: After the initialization process there are #{reactor_part1.total_lights_on} lights on"
+
+input_part2 = File.open(File.join(File.dirname(__FILE__), "part2-input.txt"), "r")
+reactor_part2 = Reactor.new
+execute_operations(input_part2, reactor_part2)
+
+puts "Day 22, Part 1: After the full reboot process there are #{reactor_part2.total_lights_on} lights on"
--- src/main/other/advent_of_code_2021/day22/reactor.rb
+++ src/main/other/advent_of_code_2021/day22/reactor.rb
@@ -2,32 +2,102 @@ require 'set'
 
 class Reactor
 
-    attr_reader :on_cubes
-
     def initialize
-        @on_cubes = Set.new
+        @cuboids = Set.new
     end
 
-    def turn_on_cuboid(x, x2, y, y2, z, z2)
-        puts "Turn on cuboid x=#{x}..#{x2}, y=#{y}..#{y2}, z=#{z}..#{z2}"
-        (x..x2).each {|x|
-            (y..y2).each {|y|
-                (z..z2).each {|z|
-                    @on_cubes.add([x, y, z])
-                }
-            }
+    def turn_on_cuboid(new_cuboid)
+        new_cuboids = [new_cuboid]
+        @cuboids.each {|cuboid|
+            new_cuboids = new_cuboids.map {|new_cuboid|
+                shared_cuboid = Reactor.shared_cuboid(cuboid, new_cuboid)
+                # Has intesection
+                if shared_cuboid[0] < shared_cuboid[1] && shared_cuboid[2] < shared_cuboid[3] && shared_cuboid[4] < shared_cuboid[5]
+                    # Split and only use non intersecting cuboids
+                    Reactor.split_and_remove_cuboid(new_cuboid, shared_cuboid)
+                else
+                    [new_cuboid]
+                end
+            }.flatten(1)
+        }
+        @cuboids = @cuboids.merge(new_cuboids)
+    end
+
+    def turn_off_cuboid(remove_cuboid)
+        new_cuboids = []
+        @cuboids.delete_if {|cuboid|
+            shared_cuboid = Reactor.shared_cuboid(cuboid, remove_cuboid)
+            # Has intersection
+            if shared_cuboid[0] < shared_cuboid[1] && shared_cuboid[2] < shared_cuboid[3] && shared_cuboid[4] < shared_cuboid[5]
+                # Split cuboid and add all parts that don't overlap to cuboids
+                new_cuboids += Reactor.split_and_remove_cuboid(cuboid, shared_cuboid)
+                true
+            else
+                false
+            end
+        }
+        @cuboids = @cuboids.merge(new_cuboids)
+    end
+
+    def total_lights_on
+        lights_on = 0
+        @cuboids.map {|x, x2, y, y2, z, z2|
+            (x2 - x) * (y2 - y) * (z2 - z)
+        }.sum
+    end
+
+    def self.shared_cuboid(cuboid1, cuboid2)
+        x, x2, y, y2, z, z2 = *cuboid1
+        a, a2, b, b2, c, c2 = *cuboid2
+
+        sx, sx2 = [a, x].max, [a2, x2].min
+        sy, sy2 = [b, y].max, [b2, y2].min
+        sz, sz2 = [c, z].max, [c2, z2].min
+
+        return sx, sx2, sy, sy2, sz, sz2
+    end
+
+    def self.split_and_remove_cuboid(cuboid1, cuboid_to_remove)
+        x, x2, y, y2, z, z2 = *cuboid1
+        a, a2, b, b2, c, c2 = *cuboid_to_remove
+
+        # Split cuboid into 27 smaller cuboids
+        Reactor.remove_planes [
+            [x, a, y, b, z, c],
+            [x, a, y, b, c, c2],
+            [x, a, y, b, c2, z2],
+            [x, a, b, b2, z, c],
+            [x, a, b, b2, c, c2],
+            [x, a, b, b2, c2, z2],
+            [x, a, b2, y2, z, c],
+            [x, a, b2, y2, c, c2],
+            [x, a, b2, y2, c2, z2],
+            # ---
+            [a, a2, y, b, z, c],
+            [a, a2, y, b, c, c2],
+            [a, a2, y, b, c2, z2],
+            [a, a2, b, b2, z, c],
+            [a, a2, b, b2, c, c2],
+            [a, a2, b, b2, c2, z2],
+            [a, a2, b2, y2, z, c],
+            [a, a2, b2, y2, c, c2],
+            [a, a2, b2, y2, c2, z2],
+            # ---
+            [a2, x2, y, b, z, c],
+            [a2, x2, y, b, c, c2],
+            [a2, x2, y, b, c2, z2],
+            [a2, x2, b, b2, z, c],
+            [a2, x2, b, b2, c, c2],
+            [a2, x2, b, b2, c2, z2],
+            [a2, x2, b2, y2, z, c],
+            [a2, x2, b2, y2, c, c2],
+            [a2, x2, b2, y2, c2, z2],
+        ] - [cuboid_to_remove]
+    end
+
+    def self.remove_planes(cuboids)
+        cuboids.select {|x, x2, y, y2, z, z2|
+            x != x2 && y != y2 && z != z2
         }
     end
-
-    def turn_off_cuboid(x, x2, y, y2, z, z2)
-        puts "Turn off cuboid x=#{x}..#{x2}, y=#{y}..#{y2}, z=#{z}..#{z2}"
-        (x..x2).each {|x|
-            (y..y2).each {|y|
-                (z..z2).each {|z|
-                    @on_cubes.delete([x, y, z])
-                }
-            }
-        }
-    end
-
 end

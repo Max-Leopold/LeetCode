1e37a8a89007edd96a520702f75df256c20845d4
Max Leopold <max.leopold2111@gmail.com>
2022-08-24 19:30:34 +0200

    Nr. 102

---
+++ src/main/problems/ruby/Nr102.rb
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[][]}
+def level_order(root, levels = [], level = 0)
+    levels << [] unless levels[level]
+    levels[level] << root
+    level_order(root.right, levels, level + 1)
+    level_order(root.left, levels, level + 1)
+    levels
+end

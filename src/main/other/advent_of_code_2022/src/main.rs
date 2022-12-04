d1959a2796a1f9f0305134ea25e1e80f368b3e73
Max Leopold <max.leopold2111@gmail.com>
2022-12-04 14:46:51 +0100

    Allow running only specific AOC days

--- src/main/other/advent_of_code_2022/src/main.rs
+++ src/main/other/advent_of_code_2022/src/main.rs
@@ -1,5 +1,10 @@
 #![feature(test)]
 
+use std::collections::HashSet;
+use std::env;
+
+use getopts::Options;
+
 extern crate test;
 
 mod day1;
@@ -8,27 +13,81 @@ mod day3;
 mod day4;
 
 fn main() {
-    println!("Day 1, Part 1: {}", day1::part1::part1().unwrap());
-    println!("Day 1, Part 2: {}", day1::part2::part2().unwrap());
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
 
-    println!("Day 1, Part 2_2: {}", day1::part2::part2_2().unwrap());
-    println!("Day 1, Part 2_3: {}", day1::part2::part2_3().unwrap());
+    let mut opts = Options::new();
+    opts.optopt(
+        "d",
+        "days",
+        "Comma seperated list of days to run. Leave empty to run all days (default)",
+        "DAY",
+    );
+    opts.optflag("h", "help", "print this help menu");
 
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            panic!("{}", f.to_string())
+        }
+    };
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let binding = matches.opt_str("d").unwrap_or_default();
+    let days = if binding.is_empty() {
+        HashSet::<u8>::from_iter(1..25)
+    } else {
+        let days = binding
+            .split(",")
+            .map(|str| str.trim().parse::<u8>().unwrap_or_default());
+        HashSet::<u8>::from_iter(days)
+    };
+
+    print_seperator();
+
+    if days.contains(&1) {
+        println!("Day 1, Part 1: {}", day1::part1::part1().unwrap());
+        println!("Day 1, Part 2: {}", day1::part2::part2().unwrap());
+
+        println!("Day 1, Part 2_2: {}", day1::part2::part2_2().unwrap());
+        println!("Day 1, Part 2_3: {}", day1::part2::part2_3().unwrap());
+
+        print_seperator();
+    }
+
+    if days.contains(&2) {
+        println!("Day 2, Part 1: {}", day2::part1::part1().unwrap());
+        println!("Day 2, Part 2: {}", day2::part2::part2().unwrap());
+
+        print_seperator();
+    }
+
+    if days.contains(&3) {
+        println!("Day 3, Part 1: {}", day3::part1::part1().unwrap());
+        println!("Day 3, Part 1_2: {}", day3::part1::part1_2().unwrap());
+        println!("Day 3, Part 2: {}", day3::part2::part2().unwrap());
+
+        print_seperator();
+    }
+
+    if days.contains(&4) {
+        println!("Day 4, Part 1: {}", day4::part1::part1().unwrap());
+        println!("Day 4, Part 2: {}", day4::part2::part2().unwrap());
+
+        print_seperator();
+    }
+}
+
+fn print_seperator() {
     println!("\n--------------------\n");
+}
 
-    println!("Day 2, Part 1: {}", day2::part1::part1().unwrap());
-    println!("Day 2, Part 2: {}", day2::part2::part2().unwrap());
-
-    println!("\n--------------------\n");
-
-    println!("Day 3, Part 1: {}", day3::part1::part1().unwrap());
-    println!("Day 3, Part 1_2: {}", day3::part1::part1_2().unwrap());
-    println!("Day 3, Part 2: {}", day3::part2::part2().unwrap());
-
-    println!("\n--------------------\n");
-
-    println!("Day 4, Part 1: {}", day4::part1::part1().unwrap());
-    println!("Day 4, Part 2: {}", day4::part2::part2().unwrap());
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
 }
 
 #[cfg(test)]

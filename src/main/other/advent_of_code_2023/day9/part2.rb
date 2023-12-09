96a7242b164783b380da9b3a1282780a853c2468
Max Leopold <max.leopold2111@gmail.com>
2023-12-09 17:19:05 +0100

    AoC 2023 Day 9 Part 2

---
+++ src/main/other/advent_of_code_2023/day9/part2.rb
@@ -0,0 +1,8 @@
+input = File.open(File.join(File.dirname(__FILE__), "input.txt"), "r").map(&:chomp).map { _1.split(" ").map(&:to_i) }
+
+def predict_seq(history)
+    diff = history.each_cons(2).map { |i, j| j - i }
+    diff.all?(&:zero?) ? 0 : diff[0] - predict_seq(diff)
+end
+
+pp input.map { _1[0] - predict_seq(_1) }.sum
